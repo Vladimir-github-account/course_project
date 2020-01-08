6143ce1f87ccb7801fde8c6c8da19f3c31e000fb
vladimir <s>
2020-01-08 13:11:39 +0200

    Add: page navigation links

--- src/assets/js/viewmodel/controls.js
+++ src/assets/js/viewmodel/controls.js
@@ -3,5 +3,5 @@
 export const header = document.getElementById('pageHeader');
 export const navToggle = document.querySelector('#pageHeader .navToggle');
 export const nav = document.querySelector('#pageHeader .pageNavigation');
-export const teamContainer = document.querySelector('div.team');
-export const clients = document.querySelector('div.clients');
+export const teamContainer = document.getElementById('team');
+export const clients = document.getElementById('clients');
